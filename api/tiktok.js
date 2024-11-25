1e898fb8cd48c55fcc11ef777d62419e32ab0880
basukimuhammad <116169432+basukimuhammad@users.noreply.github.com>
2024-11-25 19:09:37 +0700

    Update tiktok.js

--- api/tiktok.js
+++ api/tiktok.js
@@ -1,27 +1,27 @@
-const puppeteer = require("puppeteer");
+const chromium = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer-core");
 
 async function TiktokDownloader(Url) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+  });
+
   const page = await browser.newPage();
 
   try {
     await page.goto("https://ttdownloader.com/");
-    await page.type("#url", Url); // Isi URL video di form
-    await page.click("#submit"); // Klik tombol unduh
-    await page.waitForSelector("#results-list"); // Tunggu hasil muncul
+    await page.type("#url", Url);
+    await page.click("#submit");
+    await page.waitForSelector("#results-list");
 
-    // Ambil hasil unduhan
     const results = await page.evaluate(() => {
       return {
-        nowatermark: document
-          .querySelector("#results-list > div:nth-child(2) a")
-          .href,
-        watermark: document
-          .querySelector("#results-list > div:nth-child(3) a")
-          .href,
-        audio: document
-          .querySelector("#results-list > div:nth-child(4) a")
-          .href,
+        nowatermark: document.querySelector("#results-list > div:nth-child(2) a").href,
+        watermark: document.querySelector("#results-list > div:nth-child(3) a").href,
+        audio: document.querySelector("#results-list > div:nth-child(4) a").href,
       };
     });
 
